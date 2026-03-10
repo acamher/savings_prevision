05aa7ba4b015fd5555740313c14be782ce0c6a39
Álvaro  Camacho Hernandez <32852760+acamher@users.noreply.github.com>
2026-03-10 20:02:10 +0100

    new minimum value added

--- prevision_gastos.py
+++ prevision_gastos.py
@@ -1,39 +1,40 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.gridspec import GridSpec 
 import matplotlib.ticker as mtick
 
+## -------- DATA --------
+year_begining = 2026
+month_begining = 4
+year_end = 2031
+minimum_savings_months = 6
 
-# ------ DATA ---------
-begining = 2025
-end = 2031
-
-salario_mensual = 1000
-salario_extra = 1500
+salario_mensual = 1300
+salario_extra = 1600   # If there is no extra salary this is 0
 ahorros = 5000
 
 gastos_mensuales = pd.DataFrame([ 
-    ["Coche (mensual)", 330, datetime.datetime(2025,1,1), datetime.datetime(2029,12,1)],
-    ["Gimnasio", 40, datetime.datetime(begining,1,1), datetime.datetime(end,12,1)],
+    ["Coche", 332, datetime.datetime(2025,1,1), datetime.datetime(2029,11,1)],
+    ["Gimnasio", 40, datetime.datetime(year_begining,month_begining,1), datetime.datetime(year_end,12,1)],
+    ["Ocio", 300, datetime.datetime(year_begining,month_begining,1), datetime.datetime(year_end,12,1)],
+    ["Hipoteca", 800, datetime.datetime(2029,2,1), datetime.datetime(year_end,12,1)]
 ], columns=['Concepto', 'Cantidad', 'Fecha_inicio', 'Fecha_final'])
 
-total_gastos_mensuales = gastos_mensuales["Cantidad"].sum(axis=0)
-
 gastos_puntuales = pd.DataFrame([
-    ["Coche (pago final)", datetime.datetime(2029,12,1), 7000],
-    ["Viaje EEUU", datetime.datetime(2026,4,15), 1000]
+    ["Trip", datetime.datetime(2026,4,15), 1000],
+    ["Car Final Payment", datetime.datetime(2029,12,10), 10000]
 ], columns=['Concepto', 'Fecha', 'Cantidad'])
 
+## -------------- Operations --------------
+## Inicializo
+total_gastos_mensuales = gastos_mensuales["Cantidad"].sum(axis=0)
 
-## Database creation
-fechas = np.arange(np.datetime64(str(begining) + "-01-01"), np.datetime64(str(end) + "-01-01"), np.timedelta64(1, 'M'), dtype='datetime64[M]').astype('datetime64[D]')
-
-d = {"Fecha": fechas, "Ahorro GASTO": np.zeros((fechas.size)), "Ahorro NO_GASTO" : np.zeros((fechas.size)), "Balance" : np.zeros((fechas.size))}
+fechas = np.arange(datetime.datetime(year_begining, month_begining, 1), datetime.datetime(year_end, 12, 1), np.timedelta64(1, 'M'), dtype='datetime64[M]').astype('datetime64[D]')
+d = {"Fecha": fechas, "Ahorro GASTO": np.zeros((fechas.size)), "Ahorro NO_GASTO" : np.zeros((fechas.size)), "Balance" : np.zeros((fechas.size)), 'Minimo' : np.zeros((fechas.size))}
 savings = pd.DataFrame(data=d)
-
-# Functions ----------------
 
 def calculateIngress(datetime64):
     # Calculo ingreso del mes (SIN GASTOS)
@@ -58,7 +59,14 @@ def calculatePayments(datetime64):
     
     return payments
 
-## ------------ OPERATIONS ------------
+def calculateMinimumSavings(datetime64):
+    expectedPayments = 0
+    # Compute expected payments in the next months
+    for month in np.arange(minimum_savings_months):
+        expectedPayments += calculatePayments(datetime64 + relativedelta(months = month - 1))
+    
+    return expectedPayments * 1.5
+    
 
 # Initialize data
 savings.loc[0, "Ahorro GASTO"] = ahorros
@@ -72,9 +80,9 @@ for index, row in savings.iterrows():
         savings.loc[index, "Ahorro GASTO"] = savings.loc[index -1, "Ahorro GASTO"] + ingreso_del_mes - gastos_del_mes
         savings.loc[index, "Ahorro NO_GASTO"] = savings.loc[index -1, "Ahorro NO_GASTO"] + ingreso_del_mes
         savings.loc[index, 'Balance'] = ingreso_del_mes - gastos_del_mes
+        savings.loc[index, 'Minimo'] = calculateMinimumSavings(pd.to_datetime(savings.loc[index, "Fecha"]))
 
-## ------------ PLOTTING ------------
-
+## --------- PLOTTING ---------
 # Config
 fig = plt.figure(figsize=(12, 8), dpi=200)
 grid = plt.GridSpec(4, 4, hspace=0.15) # Reduced hspace for a tighter look
@@ -92,6 +100,7 @@ main_ax.yaxis.set_major_formatter(tick)
 
 main_ax.plot(savings['Fecha'], savings['Ahorro GASTO'], label='Ahorro', linewidth=2)
 main_ax.plot(savings['Fecha'], savings['Ahorro NO_GASTO'], label='Teorico', linestyle = '--', linewidth=2)
+main_ax.plot(savings['Fecha'], savings['Minimo'], label='Minimum', linestyle = ':', color = 'red', linewidth=1)
 main_ax.legend(loc='upper left', frameon=True)
 
 # --- Style Balance Plot (Bottom) ---
@@ -114,6 +123,21 @@ for ax in [main_ax, x_hist]:
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
+# --- Add Trimester Vertical Lines ---
+# Generate dates for the start of each quarter within your data range
+start_date = savings['Fecha'].min()
+end_date = savings['Fecha'].max()
+trimesters = pd.date_range(start=start_date, end=end_date, freq='QS') # 'QS' = Quarter Start
+years = pd.date_range(start=start_date, end=end_date, freq='YS') # 'YS' = Year Start
+
+for ax in [main_ax, x_hist]:
+    for date in trimesters:
+        ax.axvline(date, color='gray', linestyle=':', linewidth=1, alpha=0.4, zorder=1)
+
+for ax in [main_ax, x_hist]:
+    for date in years:
+        ax.axvline(date, color='gray', linestyle='-', linewidth=1, zorder=0)
+
 # Finally
-fig.savefig("test.png", bbox_inches='tight')
+fig.savefig("savings.png", bbox_inches='tight')
 plt.show()
